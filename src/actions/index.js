9e161a680b907d2d87d08baa16c359e9ac28ca66
Roman <pelyarich@gmail.com>
2018-02-26 20:21:25 +0200

    trying of async/await

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,19 @@ export const ADD_USER = 'ADD_USER';
 export const DELETE_USER = 'DELETE_USER';
 export const EDIT_USER = 'EDIT_USER';
 
-export const getUsers = () => dispatch => {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => dispatch({
-      type: GET_USERS,
-      payload: users,
-    }));
+export const getUsers = () => async dispatch => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+  dispatch({
+    type: GET_USERS,
+    payload: users,
+  });
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(response => response.json())
+  //   .then(users => dispatch({
+  //     type: GET_USERS,
+  //     payload: users,
+  //   }));
 };
 
 export const addUser = name => ({
