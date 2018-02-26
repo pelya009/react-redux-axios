ff6350a96958b0a3715edd02eba9d766df27e454
Roman <pelyarich@gmail.com>
2018-02-26 20:48:03 +0200

    try/catch

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,18 @@ export const DELETE_USER = 'DELETE_USER';
 export const EDIT_USER = 'EDIT_USER';
 
 export const getUsers = () => async dispatch => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await response.json();
-  dispatch({
-    type: GET_USERS,
-    payload: users,
-  });
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    console.log(users);
+    dispatch({
+      type: GET_USERS,
+      payload: users,
+    });
+  } catch (error) {
+    throw new Error('Data was not recieved from server');
+  }
+
   // fetch('https://jsonplaceholder.typicode.com/users')
   //   .then(response => response.json())
   //   .then(users => dispatch({
